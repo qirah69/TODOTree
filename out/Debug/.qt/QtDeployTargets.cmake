66c63a22050cdaea09af5f02bd4d38d8f3297a94
qirah70 <merluscastefan10@gmail.com>
2026-07-23 03:23:14 +0300

    Remove build output directory and generated json files

--- out/Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_TaskTreeQt_FILE /home/qirah70/Code/tracker/out/Debug/TaskTreeQt)
-set(__QT_DEPLOY_TARGET_TaskTreeQt_TYPE EXECUTABLE)
--- out/Debug/CMakeFiles/TaskTreeQt.dir/TaskTreeQt_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TaskTreeQt.dir/TaskTreeQt_autogen/mocs_compilation.cpp.o: \
- /home/qirah70/Code/tracker/out/Debug/TaskTreeQt_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
